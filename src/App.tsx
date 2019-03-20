edf5f982e47b5e6d0354b2f4852755e46e701a5e
Glen Coffey <glenrcoffey@gmail.com>
2019-03-21 06:58:25 +1100

    Start initial layout
    Note: This push it to change computers.

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,18 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <h1>Cell Simulator</h1>
+          <div className="flex-grid-container">
+            <div className="flex-grid"></div>
+            <div className="flex-grid"></div>
+            <div className="flex-grid"></div>
+            <div className="flex-grid"></div>
+            <div className="flex-grid"></div>
+            <div className="flex-grid"></div>
+            <div className="flex-grid"></div>
+            <div className="flex-grid"></div>
+            <div className="flex-grid"></div>
+            <div className="flex-grid"></div>
+          </div>
         </header>
       </div>
     );
